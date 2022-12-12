2c0e5b5feb3c34feec175d8bcd4075f664a48da2
themd360 <themd360@gmail.com>
2022-12-12 10:30:58 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	pust("\Programing is like building a multilingual puzzle")
+	return (0);
+}
